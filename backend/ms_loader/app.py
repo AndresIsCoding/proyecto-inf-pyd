dc18db950e271ab6e0e88d68382847aae41c1b7b
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2025-07-18 22:14:50 -0500

    Dataset s&p 500 y reconfiguracion de ms_loader

--- backend/ms_loader/app.py
+++ backend/ms_loader/app.py
@@ -1,30 +1,29 @@
 from fastapi import FastAPI
 import pandas as pd
+import yfinance as yf
+import os
 
 app = FastAPI()
 
-# Carga del dataset
-df = pd.read_csv('backend\ms_loader\HousingData.csv')
+# Cargar el listado de empresas
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+sp500 = sp500.dropna()
+sp500['Symbol'] = sp500['Symbol'].str.replace('.','-')
+symbol_list = sp500['Symbol'].unique().tolist()
 
-# Eliminación de las filas con valores NaN
-df = df.dropna()
+# Fechas 
+end_date = '2023-09-27'
+start_date = pd.to_datetime(end_date)-pd.DateOffset(365*8)
 
-# Eliminacion de las variables que no se van a utilizar
-df = df.drop(columns=['B', 'CHAS', 'ZN', 'INDUS', 'RAD'])
+# Construccion del dataset
 
-# Seleccion de variables para el entrenamiento
-features = ["RM","LSTAT", "PTRATIO", "DIS", "TAX"]
-target = "MEDV"
+df = yf.download(tickers=symbol_list,start=start_date,end=end_date).stack()
+df.index.names = ['date','ticker']
+df.columns = df.columns.str.lower()
 
-x = df[features]
-y = df[target]
-
-# Dataset ya procesado y exportado
-df_final = df[features + [target]]
-
+# Dataset listo para ser pasado a ms_stats
 @app.get("/dataset")
 def get_processed_dataset():
-    return df_final.to_dict(orient="records")
+    return df.reset_index().to_dict(orient="records")
 
-
-print(df_final.head())
+print(df.head())
